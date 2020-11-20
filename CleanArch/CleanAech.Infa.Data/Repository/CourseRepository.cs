6881cf918cb2b3ebab9e59a9d0e6e6e950f124be
alirezaabediniDEV <72316722+alirezaabediniDEV@users.noreply.github.com>
2020-11-20 17:10:17 +0330

    Course Repository

---
+++ CleanArch/CleanAech.Infa.Data/Repository/CourseRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CleanAech.Infa.Data.Context;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+
+namespace CleanArch.Infa.Data.Repository
+{
+    public class CourseRepository:ICourseRepository
+    {
+        private UniversityDBContext _context;
+
+        public CourseRepository(UniversityDBContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Course> GetCourses()
+        {
+            return _context.Courses;
+        }
+    }
+}
